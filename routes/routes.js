452320f2ef40bc1f7662ae0050b5208eea2f145d
Alice Alsford <alice.alsford@gmail.com>
2019-10-26 16:42:25 +1300

    post request to json from have

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const wantData = require('../wanter.json')
+const haveData = require('../donator.json')
 const fs = require('fs')
 // router.get('/', function (req, res) {
 //     res.redirect('/want')
@@ -38,6 +39,29 @@ router.post('/want', (req, res) => {
     fs.writeFileSync('wanter.json', editedData)
 
     res.render('want')
+})
+
+router.post('/have', (req, res) => {
+    let newDonator = {
+        name: req.body.name,
+        location: req.body.location,
+        email: req.body.email,
+        description: req.body.description,
+        image: req.body.image
+    }
+
+    let currentData = {
+        info: haveData
+    }
+
+    currentData.info.donator.push(newDonator)
+
+    let editedData = JSON.stringify(currentData, null, 2)
+
+
+    fs.writeFileSync('donator.json', editedData)
+
+    res.render('have')
 })
 
 module.exports = router
