2ce152efc69fc86aed444f89d77f7c2cbf2b4fc5
Alice Alsford <alice.alsford@gmail.com>
2019-10-26 14:13:30 +1300

    set up routes and main hbs

---
+++ routes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+// router.get('/', function (req, res) {
+//     res.redirect('/want')
+// })
+
+router.get('/want', function (req, res) {
+    res.send("this is the page that will have the wants")
+})
+
+router.get('/have', function (req, res) {
+    res.send("this is the page that will have the haves")
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express')
-const hbs = require('handlebars')
+const hbs = require('express-handlebars')
+const routes = require('./routes')
 const server = express()
 
+//Middleware
+server.engine('hbs', hbs({
+    defaultLayout: 'main',
+    extname: 'hbs'
+}))
+server.set('view engine', 'hbs')
+server.use(express.static('public'))
+server.use(express.urlencoded({ extended: false }))
+
+
+//home page route
 server.get('/', (req, res) =>
     res.send("Hello World"))
+
+//Routers
+server.use('/', routes)
+
+
 
 module.exports = server
